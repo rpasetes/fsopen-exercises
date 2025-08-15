cb0b5c2a860ac4c1a234c5ab7f391273bc88be6e
rpasetes <russap01@gmail.co>
2025-08-15 11:01:25 -0400

    exercise 1.12 anecdotes checkpoint

--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -2,6 +2,6 @@ import ReactDOM from 'react-dom/client'
 
 // route based on exercise parts
 // eg. anecdotes/App or unicafe/App
-import App from './../unicafe/App'
+import App from './../anecdotes/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
