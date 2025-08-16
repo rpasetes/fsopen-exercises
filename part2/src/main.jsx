133cf177854e69f2c654addeb77a07fdbfae8150
rpasetes <russap01@gmail.co>
2025-08-16 15:07:31 -0400

    initializing part2 exercises directory

---
+++ part2/src/main.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client'
+
+// route based on exercise parts
+// eg. anecdotes/App or unicafe/App
+import App from './../courseinfo/App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
