dcf2022b66eeee7a6dd2062605796304e906b5d2
rpasetes <russap01@gmail.co>
2025-08-16 17:23:48 -0400

    exercise 2.2+3 courseinfo checkpoint

--- part2/courseinfo/components/Course.jsx
+++ part2/courseinfo/components/Course.jsx
@@ -1,13 +1,11 @@
 const Header = ({ name }) => {
-  console.log(name)
-  
   return (
     <h1>{name}</h1>
   )
 }
 
 const Part = (props) => {
-  console.log("part receiving props", props);
+  // console.log("part receiving props", props);
   
   return (
     <p>
@@ -17,7 +15,7 @@ const Part = (props) => {
 }
 
 const Content = ({ parts }) => {
-  console.log('content receiving parts', parts);
+  //console.log('content receiving parts', parts);
   
   return (
     <div>
@@ -26,11 +24,38 @@ const Content = ({ parts }) => {
   )
 }
 
-// const Total = (props) => {
-//   return (
-//     <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-//   )
-// }
+const Total = ({ parts }) => {
+  console.log('total parts', parts)
+  // const total = parts.reduce(
+  //   // currentValue accesses the field to be reduced
+  //   (accumulator, currentValue) => accumulator + currentValue.exercises,
+  //   // supplying an initialValue of 0 to accumulator
+  //   0,
+  // )
+
+  // const total = parts.reduce((a, c) => {
+  //   // FOR DEBUGGING PURPOSES:
+  //   // make sure you return a value in the arrow
+  //   // function body or values get dropped
+  //   // and accumulator prints to 'undefined'
+  //   // (lost 30 minutes to this shizz...)
+  //   console.log('what is happening', a, c)
+  //   return a + c.exercises
+  //   },
+  //   0,
+  // )
+
+  // console.log('parts total', total)
+
+  return (
+    <p>
+      <b>
+        total of {parts.reduce((a, c) => a + c.exercises, 0)} exercises
+      </b>
+    </p>
+    // <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+  )
+}
 
 const Course = ({ course }) => {
   console.log('inheriting course', course);
@@ -39,7 +64,7 @@ const Course = ({ course }) => {
     <div>
       <Header name={course.name} />
       <Content parts={course.parts} />
-      {/* <Total parts={course.parts} /> */}
+      <Total parts={course.parts} />
     </div>
   )
 }
