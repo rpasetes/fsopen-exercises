5d92e0068b696f34aeab9f1c0aeef7a767f98fff
rpasetes <russap01@gmail.co>
2025-08-18 12:56:31 -0400

    exercise 2.8 phonebook checkpoint

--- part2/phonebook/App.jsx
+++ part2/phonebook/App.jsx
@@ -1,52 +1,65 @@
 import { useState } from 'react'
 
-const Contacts = ({ persons }) => {
+// (12:50) aight new contact component,
+// let's see if the changes propagate.
+const Contact = ({ person }) => {
   return (
     <div>
-      {persons.map((person) => <div key={person.name}>{person.name}</div>)}
+      {person.name} {person.number}
+    </div>
+  )
+}
+
+const Contacts = ({ persons }) => {
+  // (12:52) remember: set 'key' prop in the map()
+  return (
+    <div>
+      {persons.map((person) => <Contact key={person.id} person={person} />)}
     </div>
   )
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { id: 1, name: 'Arto Hellas' }
-  ])
+  // (12:47) okay now we adding numbers,
+  // gotta update Contacts display
+  const [persons, setPersons] = useState([{
+    id: 1, 
+    name: 'Arto Hellas', 
+    number: '040-1234567',
+  }])
   const [newName, setNewName] = useState('type new name...')
+  // (12:44) yea we adding numbers now bb
+  const [newNumber, setNewNumber] = useState('type new number...')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   // check for existing names here
   const onSubmit = (event) => {
     event.preventDefault()
 
-    // kinda wanna try to filter 'persons'
-    // to call includes() on...
-    // (10 mins later) nvm: let's not jump ahead. 
-    // let's try removing id set from prev exercise
-    // and call includes() by matching objects...
-    // remember to change keys on ContactList.
-    // (12:33) hmm includes() doesn't seem to pass check
-    // (12:37) okay since we're working with objects,
-    // gemini search seems to recommend some(), since
-    // it's able to test each arr element w/ a function
-    // (12:40) hell yea it works! which means i can also
-    // add ids back to the initial 'persons' data struct,
     if (persons.some((person) => person.name === newName)) {
-      // remember: template strings insert with ${}
       alert(`${newName} is already added to phonebook`)
       return
     }
 
+    // (12:53) aight, new object formed with
+    // the field change, now saving to check...
+    // (12:55) phew 1-800-smackeditup,
     const newPerson = {
       id: String(persons.length + 1),
       name: newName,
+      number: newNumber,
     }
 
     setPersons(persons.concat(newPerson))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
@@ -57,6 +70,12 @@ const App = () => {
           name: <input 
           value={newName} 
           onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          number: <input
+          value={newNumber}
+          onChange={handleNumberChange}
           />
         </div>
         <div>
