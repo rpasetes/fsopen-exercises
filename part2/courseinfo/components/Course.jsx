0bb07c81b53cfdbeef3126d3025986921b84a41a
rpasetes <russap01@gmail.co>
2025-08-17 20:34:40 -0400

    refactoring Course.jsx

--- part2/courseinfo/components/Course.jsx
+++ part2/courseinfo/components/Course.jsx
@@ -1,21 +1,8 @@
-const Header = ({ name }) => {
-  return (
-    <h1>{name}</h1>
-  )
-}
+const Header = ({ name }) => <h1>{name}</h1>
 
-const Part = (props) => {
-  // console.log("part receiving props", props);
-  
-  return (
-    <p>
-      {props.name} {props.exercises}
-    </p>
-  )
-}
+const Part = ({ name, exercises }) => <p>{name} {exercises}</p>
 
 const Content = ({ parts }) => {
-  //console.log('content receiving parts', parts);
   
   return (
     <div>
@@ -25,40 +12,19 @@ const Content = ({ parts }) => {
 }
 
 const Total = ({ parts }) => {
-  console.log('total parts', parts)
-  // const total = parts.reduce(
-  //   // currentValue accesses the field to be reduced
-  //   (accumulator, currentValue) => accumulator + currentValue.exercises,
-  //   // supplying an initialValue of 0 to accumulator
-  //   0,
-  // )
-
-  // const total = parts.reduce((a, c) => {
-  //   // FOR DEBUGGING PURPOSES:
-  //   // make sure you return a value in the arrow
-  //   // function body or values get dropped
-  //   // and accumulator prints to 'undefined'
-  //   // (lost 30 minutes to this shizz...)
-  //   console.log('what is happening', a, c)
-  //   return a + c.exercises
-  //   },
-  //   0,
-  // )
-
-  // console.log('parts total', total)
-
+  // console.log('totalling parts', parts)
+  
   return (
     <p>
       <b>
         total of {parts.reduce((a, c) => a + c.exercises, 0)} exercises
       </b>
     </p>
-    // <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   )
 }
 
 const Course = ({ course }) => {
-  console.log('inheriting course', course);
+  // console.log('inheriting course', course);
   
   return (
     <div>
