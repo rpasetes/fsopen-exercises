82806869a5ef71702bd777382898d948078a7b38
rpasetes <russap01@gmail.co>
2025-08-24 13:43:10 -0400

    3a deleting resources notes

--- part3/index.js
+++ part3/index.js
@@ -27,20 +27,22 @@ app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
 
-// (1321) tyty colon syntax for routing, maps to req.params
-// (1324) can also confirm auto change tracking works upon save!
 app.get('/api/notes/:id', (request, response) => {
   const id = request.params.id
   const note = notes.find(note => note.id === id)
-  // (1326) hehe guard pattern bb, for undef notes
-  // (1328) ohh nvm guard pattern doesn't work cuz of: 
-  // Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers 
-  // after they are sent to the client, have to if-else
   if (note) {
     response.json(note)
   } else {
     response.status(404).end()
   }
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id
+  notes.filter(note => note.id === id)
+
+  // (1341) in this case, task can also fail successfully,
+  res.status(204).end()
 })
 
 const PORT = 3001
