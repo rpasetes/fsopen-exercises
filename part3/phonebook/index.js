475e93cc68f2d799c9298b4a6b88999f14f63588
rpasetes <russap01@gmail.co>
2025-08-25 12:56:16 -0400

    exercise 3.5 phonebook backend checkpoint

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+// (1228) okay first things first, json request middleware
+app.use(express.json())
+
 contacts = [
   { 
     "id": "1",
@@ -48,18 +51,44 @@ app.get('/api/persons/:id', (request, response) => {
   response.json(contact)
 })
 
-// (1212) aight we back, n'yea lul u can just overwrite contacts
-// (1214) and end the response status with 204, lul again
-// (1215) meta: retain both material and exercises within 24 hrs
-// (1218) whoops, filtered all contacts BUT id param haha (used ===)
-// (1219) nice, rest client deletes the number! thx console.log()
-// (1222) and it can take multiple delete requests heck yea! ship
 app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id
   contacts = contacts.filter(c => c.id !== id)
 
   response.status(204).end()
-  console.log(contacts)
+  // console.log(contacts)
+})
+
+// (..1228) templating from "Getting random integer between
+// two values" in random() Mozilla docs
+// (1250) oop okay, the id is also a String in the data, 
+const generateId = () => {
+  const ceil = 1048576
+  const floor = 4
+  return String(Math.floor(Math.random() * (ceil - floor) + floor))
+} 
+
+// (1232) aight we don't need to do error handling yet,
+// (1239) okay lmao having the data named 'contacts' kinda confuse
+// (1244) whoop twitter break + emotional attuning break; back to it.
+// (1246) wait haha, make a contact w/ the appropriate props; not notes
+// (1249) OMFG Remember: forward slash before any route, esp /api/...
+// (1252) phew AMAZING contacts updating with new numbers!
+// (1254) cool, shaping the data to match the other resources; ship!
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  
+  const contact = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  }
+
+  console.log('creating new contact', contact)
+
+  contacts = contacts.concat(contact)
+  response.json(contact)
+  console.log('contacts updating to:', contacts)
 })
 
 const PORT = 3001
