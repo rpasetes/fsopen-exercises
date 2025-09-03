f421ef8233c16e5aca3e8432597c3c3c88bc698b
rpasetes <russap01@gmail.co>
2025-09-03 11:58:37 -0400

    exercise 3.12 cli phonebook database checkpoint

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,59 @@
+// (1148) damn that was fuckin' straightforward! good returns yall
+// notes: checking equality for process.argv.length to guard missing data
+// changed appname to specify phonebook in mongo url
+// Remember: run mongoose.connection.close *within* .then() event handler!
+// but tbh great work, SHIP!
+
+const mongoose = require('mongoose')
+
+if (process.argv.length === 2) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = 
+`mongodb+srv://russap01_db_user:${password}@cluster0.dccws8n.mongodb.net/phonebookApp?
+retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
+
+if (process.argv.length === 4) {
+  console.log('give person number as argument')
+  process.exit(1)
+}
+
+if (process.argv.length === 5) {
+  const newName = process.argv[3]
+  const newNumber = process.argv[4]
+
+  const person = new Person({
+    name: newName,
+    number: newNumber,
+  })
+
+  person.save().then(result => {
+    console.log(`added ${newName} number ${newNumber} to phonebook`)
+    mongoose.connection.close()
+  })
+}
